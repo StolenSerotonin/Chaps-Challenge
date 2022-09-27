2d99943c00a2078fb101afe9f1abb8257db14f0b
Tomin Jessil <jessiltomi@ecs.vuw.ac.nz>
2022-09-27 16:08:34 +1300

    JUnit added. Persistency now can load levels. Can't load inventory yet. A test file has been added.

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
@@ -1,7 +1,8 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
 
-import nz.ac.vuw.ecs.swen225.gp22.domain.FloorTile;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Level;
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Door.DoorC;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Key.KeyC;
 
 import java.io.File;
 import java.io.IOException;
@@ -38,23 +39,70 @@ public class Persistency {
         List<Element> rowsList = rootElement.getChildren();
         
         //Iterating through all the rows
-        //TODO "Iterate through each each tile per row, check what each tile is .contains (check colour aswell),then make apporitate object for the position in array in newBoard
         String[][] array = new String[ROWS][COLUMNS];
         for(int y = 0; y < ROWS; y++){
             Element row = rowsList.get(y);
             List<Element> tiles = row.getChildren("tile");
             for(int x = 0; x < COLUMNS; x++){
                 String tileText = tiles.get(x).getText();
-                array[y][x] = tileText;
+                if(tileText.contains("wall") || tileText.contains("floor") || tileText.contains("exit")){
+                    newLevel.setTile(y, x, getTile(tileText, y, x));
+                }
+                else{
+                    newLevel.setTile(y, x, new FloorTile(y, x));
+                    newLevel.setObject(y, x, getSolidObject(tileText, y, x));
+                }
             }
         }
         return newLevel;
     }
 
-    public static void saveBoard(){
-        
-    }
+    public static void saveBoard(){}
 
+    public static Tile getTile(String tile, int xPos, int yPos){
+        Tile tileObject = null;
+        switch(tile){
+            case "wall":
+                tileObject = new WallTile(xPos, yPos);
+                break;
+            case "floor":
+                tileObject = new FloorTile(xPos, yPos);
+                break;
+            case "exit":
+                tileObject = new Exit(xPos, yPos);
+                break;
+            default:
+                System.out.println("Error Constructing Tile: " + tile + " at " + "X: " + xPos + " Y: " + yPos);
+                break;
+        }
+        return tileObject;
+    }
+        
+    public static SolidObject getSolidObject(String solidObj, int xPos, int yPos){
+        SolidObject sObject = null;
+        if(solidObj.contains("Key")){
+            if(solidObj.contains("Yellow")){sObject = new Key(xPos, yPos, KeyC.YELLOW);}
+            else if(solidObj.contains("Red")){sObject = new Key(xPos, yPos, KeyC.RED);}
+            else if(solidObj.contains("Blue")){sObject = new Key(xPos, yPos, KeyC.BLUE);}
+            else if(solidObj.contains("Green")){sObject = new Key(xPos, yPos, KeyC.GREEN);}
+        }
+        else if(solidObj.contains("Door")){
+            if(solidObj.contains("Yellow")){sObject = new Door(xPos, yPos, DoorC.YELLOW);}
+            else if(solidObj.contains("Red")){sObject = new Door(xPos, yPos, DoorC.RED);}
+            else if(solidObj.contains("Blue")){sObject = new Door(xPos, yPos, DoorC.BLUE);}
+            else if(solidObj.contains("Green")){sObject = new Door(xPos, yPos, DoorC.GREEN);}
+        }
+        else if(solidObj.contains("exitLock")){
+            sObject = new ExitLock(xPos, yPos);
+        }
+        else if(solidObj.contains("computerChip")){
+            sObject = new ComputerChip(xPos, yPos);
+        }
+        else{
+            System.out.println("Error Constructing Solid object: " + solidObj + " at " + "X: " + xPos + " Y: " + yPos);
+        }
+        return sObject;
+    }
 
 
 }
---
+++ test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTests.java
@@ -0,0 +1,27 @@
+package nz.ac.vuw.ecs.swen225.gp22.persistency;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.persistency.*;
+
+import java.io.IOException;
+
+import org.jdom2.JDOMException;
+import org.junit.Test;
+
+
+public class PersistencyTests {
+    @Test
+    public void test1() throws JDOMException, IOException{
+        //TODO Make the test print out the arrays stored in the level
+        // int ROWS = 20;
+        // int COLUMNS = 21;
+        // Level level1 = Persistency.loadBoard("Level1.xml");
+        // for(int i = 0; i < ROWS; i++){
+        //     System.out.println("| ");
+        //     for(int j = 0; j < COLUMNS; j++){
+        //         System.out.print(array[i][j].charAt(0) + "| ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
