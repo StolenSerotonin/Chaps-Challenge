d767b8bf71247d39f0962f19ae5637324a4c0d92
tomasdomi <tomasdomi@ecs.vuw.ac.nz>
2022-09-29 02:52:43 +1300

    FuzzTest made, get rid of fuzztest in  src

---
+++ test/nz/ac/vuw/ecs/swen225/gp22/FuzzTest.java
@@ -0,0 +1,10 @@
+package nz.ac.vuw.ecs.swen225.gp22;
+
+import nz.ac.vuw.ecs.swen225.gp22.*;
+import org.junit.Test;
+public class FuzzTest {
+    @Test
+    public void test1(){
+        
+    }
+}
