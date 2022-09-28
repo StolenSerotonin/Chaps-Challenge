60a24f755f14412a08c507378746a9ab8967557b
tomasdomi <tomasdomi@ecs.vuw.ac.nz>
2022-09-29 02:51:04 +1300

    Making FuzzTest file within test folder

---
+++ test/nz/ac/vuw/ecs/swen225/gp22/FuzzTests.java
@@ -0,0 +1,10 @@
+package nz.ac.vuw.ecs.swen225.gp22;
+
+import nz.ac.vuw.ecs.swen225.gp22.*;
+import org.junit.Test;
+public class FuzzTests {
+    @Test
+    public void test1(){
+        
+    }
+}
