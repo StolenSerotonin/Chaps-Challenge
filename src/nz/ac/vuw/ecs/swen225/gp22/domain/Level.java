32f3f58a2a1b647433c00c7dbafb4470fa50c9f4
Liam Mcrobbie <mcrobbliam@ecs.vuw.ac.nz>
2022-10-03 20:36:33 +1300

    STATIC

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Level.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Level.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import java.util.Iterator;
 
 public class Level implements Iterable<Point>{
-	private Tile[][] tiles;
+	private static Tile[][] tiles;
 	private SolidObject[][] objects;
 	private Point startingPosition;
 	private Point chapPosition;
