fe5a4fe5320cf471478a5fffc12f2979ace1cc64
tomasdomi <tomasdomi@ecs.vuw.ac.nz>
2022-10-04 17:01:03 +1300

    Making random Direction never the opposite of prev Direction

--- test/nz/ac/vuw/ecs/swen225/gp22/FuzzTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp22/FuzzTest.java
@@ -10,97 +10,111 @@ import org.junit.Test;
 
 enum Direction{
     UP{
-        int direction(){
+        int keyInt(){
             return KeyEvent.VK_UP;
         }
         Direction opp(){
             return DOWN;
         }
+        void printString(){
+            System.out.println("UP");
+        }
     },
     DOWN{
-        int direction(){
+        int keyInt(){
             return KeyEvent.VK_DOWN;
         }
         Direction opp(){
             return UP;
         }
+        void printString(){
+            System.out.println("DOWN");
+        }
     },
     RIGHT{
-        int direction(){
+        int keyInt(){
             return KeyEvent.VK_RIGHT;
         }    
         Direction opp(){
             return LEFT;
         }
+        void printString(){
+            System.out.println("RIGHT");
+        }
     },
     LEFT{
-        int direction(){
+        int keyInt(){
             return KeyEvent.VK_LEFT;
         }
         Direction opp(){
             return RIGHT;
         }
+        void printString(){
+            System.out.println("RIGHT");
+        }
     };
-
     Direction opp(){
         return this.opp();
     }
-    int direction(){
-        return this.direction();
+    int keyInt(){
+        return this.keyInt();
     }
-    List<Direction> directionsNotOpp(){
+    List<Direction> notOppDir(){
         return Arrays.asList(Direction.values()).stream().filter(d -> d != this.opp()).collect(Collectors.toList());
     }
+    void printString(){
+        this.printString();
+    }
 }
-
 public class FuzzTest{
-
-
-    int keys [] = { KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT};
-    
     Random rand = new Random();
     @Test
     public void FuzzTest(){
         try {
-            test1();
-            test2();
+            testLvl1();
+            testLvl2();
         } catch (Throwable e) {
             e.printStackTrace();
             System.out.println("ERROR");
         }
     }
-
-    public void test1 () throws AWTException, Throwable{
+    public void testLvl1 () throws AWTException, Throwable{
         GUI gui1 = new GUI("Chap's Challenge", 800, 600, 1);
         System.out.println("TESTING LEVEL 1\n---------------------------------");
         Robot rob = new Robot();
-        for(int i = 0; i < 120; ++i){
-            int key = keys[rand.nextInt(4)];
+        Direction preDir = Direction.values()[rand.nextInt(4)];
+        for(int i = 0; i < 120; ++i){ //created robot presses random key 120 times every half second
+            Direction key = preDir.notOppDir().get(rand.nextInt(3)); //Never uses opposite direction as previous
+            preDir = key;
             System.out.println(i+"\nKEY:"+ key);
             rob.delay(500);
-            rob.keyPress(key);
+            rob.keyPress(key.keyInt());
+            System.out.println(key.notOppDir());
+            System.out.println("");
         }
     }
-    public void test2() throws AWTException, Throwable{
+    /**
+     * @throws AWTException
+     * @throws Throwable
+     * Tests the second level
+     * Runs the game, press random arrow keys
+     */
+    public void testLvl2() throws AWTException, Throwable{
         GUI gui2 = new GUI("Chap's Challenge", 800, 600, 2);
         System.out.println("\nTESTING LEVEL 2\n---------------------------------");
         Robot rob = new Robot();
-            for(int i = 0; i < 120; i++){
-                int key = keys[rand.nextInt(4)];
+        Direction preDir = Direction.values()[rand.nextInt(4)];
+            for(int i = 0; i < 120; i++){//created robot presses random key 120 times every half second
+                Direction key = preDir.notOppDir().get(rand.nextInt(3)); //Never uses opposite direction as previous
+                preDir = key;
                 System.out.println(i+"\nKEY:"+ key);
                 rob.delay(500);
-                rob.keyPress(key);
+                rob.keyPress(key.keyInt());
+                System.out.println(key.notOppDir());
+                System.out.println("");
+
+                
+
             }
     }
 }
-
-        // KeyEvent[] keyEvents1 = {
-        //     new KeyEvent(gui1, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_UP,'A'),
-        //     new KeyEvent(gui1, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_DOWN,'B'),
-        //     new KeyEvent(gui1, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT,'C'),
-        //     new KeyEvent(gui1, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT,'D')
-        // };
-        
-        // Helper helper1 = new Helper();
-        // Timer timer1 = new Timer();
-        // timer1.schedule(helper1, 1000, 10000);
