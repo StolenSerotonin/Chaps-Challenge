6315cd0401ef5e4ee6d086b93209813d6c8e5244
mcrobbliam <mcrobbliam@myvuw.ac.nz>
2022-10-13 20:41:26 +1300

    Enemy is now a SolidObject 2

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Enemy.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Enemy.java
@@ -1,9 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
+import nz.ac.vuw.ecs.swen225.gp22.renderer.Images;
+
 /*
  * Class for enemy actor
  */
-public class Enemy {
+public class Enemy extends SolidObject{
     private int x, y, xPos, yPos; 
 	private int lastXPos, lastYPos; 
 	private int targetY1, targetY2, currentTarget;
@@ -20,6 +22,7 @@ public class Enemy {
 		this.targetY1 = yPos + 2;
 		this.targetY2 = yPos - 2;
 		this.currentTarget = 1;
+		setImg(Images.Enemy);
 		x = xPos * 24; //decided on 24 by carefull maths
 		y = yPos * 24;
 		direction = Direction.DOWN;
@@ -66,8 +69,7 @@ public class Enemy {
 		direction = dir;
 	}
 	/*
-	 * Enemy last Xpos on the board
-	 */
+	 * Enemy last Xpos on the boardhttps://gitlab.ecs.vuw.ac.nz/course-work/swen225/2022/project1/t23/chaps-challenge into Liams-Domain
 	public int getLastXPos(){
 		return lastXPos;
 	}
@@ -85,6 +87,7 @@ public class Enemy {
 		if(!level.getTile(xPos+dx, yPos+dy).isPassable()) {
 			throw new IllegalArgumentException("Chap cannot phase through walls");
 		}
+		lastXPos = xPos;
 		lastYPos = yPos;
 		xPos += dx;
 		yPos += dy;
@@ -136,5 +139,18 @@ public class Enemy {
 	 */
 	public String toString(){
 		return "enemy";
+	}
+
+	@Override
+	public void onCollision(Chap c) {
+		c.getState().die();
+		
+	}
+
+	@Override
+	public void initialize() {
+		setImg(Images.Enemy);
+		setCollided(false);
+		
 	}
 }
