8fac7f6f1a55d197b4fbffb9af848b405bf23c5a
jessiltomi <jessiltomi@ecs.vuw.ac.nz>
2022-10-15 16:43:53 +1300

    Commit

--- test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTests.java
+++ test/nz/ac/vuw/ecs/swen225/gp22/persistency/PersistencyTests.java
@@ -7,34 +7,176 @@ import java.io.IOException;
 import org.jdom2.JDOMException;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class PersistencyTests {
+    public String levelsURL = "src/nz/ac/vuw/ecs/swen225/gp22/persistency/levels/";
+    public String savedGamesURL = "src/nz/ac/vuw/ecs/swen225/gp22/persistency/savedGames/";
+
     @Test
+    //Testing game loading with level1
     public void test1() throws JDOMException, IOException{
+        String expected =
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | W | W | W | W | F | W | W | W | W | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | F | F | W | W | W | F | F | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | C | F | W | E | W | F | C | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | D | W | L | W | D | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | K | F | D | F | F | F | F | F | D | F | K | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | I | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | C | F | F | F | C | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | F | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | F | F | D | F | F | F | F | F | D | F | F | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | W | D | W | D | W | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | F | W | F | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | C | W | C | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | K | F | W | K | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | W | W | W | W | W | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n";
+        String actual = "";
         try {
-            Level level1 = Persistency.loadBoard("level1.xml");
-            check(level1);
-            Persistency.saveBoard(level1);
+            Level level1 = Persistency.loadBoard("level1.xml", levelsURL);
+            actual = makeStringBoard(level1);
+            assertEquals("Actual matches expected",expected, actual);
         }
         catch (Exception e) {
-            e.printStackTrace(); 
+            e.printStackTrace();
         }
-        
+    }
+    @Test
+    //Testing game saving
+    public void test2() {
+        String expected =
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | W | W | W | W | F | W | W | W | W | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | F | F | W | W | W | F | F | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | C | F | W | E | W | F | C | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | D | W | L | W | D | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | K | F | D | F | F | F | F | F | D | F | K | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | I | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | C | F | F | F | C | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | F | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | F | F | D | F | F | F | F | F | D | F | F | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | W | D | W | D | W | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | F | W | F | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | C | W | C | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | K | F | W | K | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | W | W | W | W | W | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n";
+        String actual = "";
+        try {
+            Level level1 = Persistency.loadBoard("level1.xml", levelsURL);
+            Chap chap = new Chap(level1.getStartingX(), level1.getStartingY(), level1);
+            Persistency.saveBoard(level1, "savedGame.xml", savedGamesURL, chap);
+            Level savedGame = Persistency.loadBoard("savedGame.xml", savedGamesURL);
+            actual = makeStringBoard(savedGame);
+            assertEquals("Actual matches expected",actual, expected);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    //Testing game loading with level1
+    public void test3() throws JDOMException, IOException{
+        String expected =
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | W | W | W | W | F | W | W | W | W | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | F | F | W | W | W | F | F | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | C | F | W | E | W | F | C | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | D | W | L | W | D | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | K | F | D | F | F | F | F | F | D | F | K | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | I | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | C | F | F | F | C | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | F | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | F | F | D | F | F | F | F | F | D | F | F | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | W | D | W | D | W | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | F | W | F | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | C | W | C | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | K | F | W | K | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | W | W | W | W | W | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n";
+        String actual = "";
+        try {
+            Level level1 = Persistency.loadBoard("level1.xml", levelsURL);
+            actual = makeStringBoard(level1);
+            assertEquals("Actual matches expected",expected, actual);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    //Testing game saving
+    public void test4() {
+        String expected =
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | W | W | W | W | F | W | W | W | W | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | F | F | W | W | W | F | F | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | W | F | C | F | W | E | W | F | C | F | W | F | F | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | D | W | L | W | D | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | K | F | D | F | F | F | F | F | D | F | K | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | I | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | C | F | F | F | C | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | W | F | C | F | W | K | F | F | F | K | W | F | C | F | W | F | F | F | \n" +
+        "| F | F | F | W | F | F | F | D | F | F | F | F | F | D | F | F | F | W | F | F | F | \n" +
+        "| F | F | F | W | W | W | W | W | W | D | W | D | W | W | W | W | W | W | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | F | W | F | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | F | C | W | C | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | K | F | W | K | F | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | W | W | W | W | W | W | W | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n" +
+        "| F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | F | \n";
+        String actual = "";
+        try {
+            Level level1 = Persistency.loadBoard("level1.xml", levelsURL);
+            Chap chap = new Chap(level1.getStartingX(), level1.getStartingY(), level1);
+            Persistency.saveBoard(level1, "savedGame.xml", savedGamesURL, chap);
+            Level savedGame = Persistency.loadBoard("savedGame.xml", savedGamesURL);
+            actual = makeStringBoard(savedGame);
+            assertEquals("Actual matches expected",actual, expected);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void check(Level l) {
+    public static String makeStringBoard(Level l) {
+        String board = "";
         for(int i = 0; i < l.getTiles().length; i++){
-            System.out.print("| ");
+            board += "| ";
             for(int j = 0; j < l.getTiles()[i].length; j++){
                 if(l.getTile(i, j) != null && l.getObject(i, j) != null){
-                    System.out.print(getSolidObject(l.getObject(i, j)) + " | ");   
+                    board += getSolidObject(l.getObject(i, j)) + " | ";
                 }
                 else{
-                    System.out.print(getTileObject(l.getTile(i, j)) + " | ");   
+                    board += getTileObject(l.getTile(i, j)) + " | ";
                 }
             }
-            System.out.println();
+            board += "\n";
         }
+        System.out.println(board);
+        return board;
     }
     public static String getTileObject(Tile tO) {
         String tileObjectStr = "";
@@ -46,6 +188,12 @@ public class PersistencyTests {
         }
         else if(tO instanceof Exit){
             tileObjectStr =  "E";
+        }
+        else if(tO instanceof InfoTile){
+            tileObjectStr =  "I";
+        }
+        else if(tO instanceof WaterTile){
+            tileObjectStr =  "A";
         }
         return tileObjectStr;
     }
@@ -61,7 +209,7 @@ public class PersistencyTests {
             solidObjectStr =  "D";
         }
         else if(sO instanceof ComputerChip){
-            solidObjectStr = "C"; 
+            solidObjectStr = "C";
         }
         return solidObjectStr;
     }
